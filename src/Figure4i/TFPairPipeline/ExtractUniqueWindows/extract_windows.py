46a6a0ae4fc8d64bbdd50930ba7423b864632b3f
Liam Speakman <ls173818@ohio.edu>
2025-07-14 08:16:11 -0400

    Removed old code files + added comments

--- src/Figure4i/TFPairPipeline/ExtractUniqueWindows/extract_windows.py
+++ src/Figure4i/TFPairPipeline/ExtractUniqueWindows/extract_windows.py
@@ -1,22 +1,31 @@
+"""
+====================================================================================================================
+Python script name extract_windows.py
+Script written by Liam Speakman
+
+Date: July 14, 2025
+
+This script takes in a contact file in the form "chrA:1-2 chrB:3-4" and produces a bed file with the present unique windows 
+
+Usage example:
+>>> python3 extract_windows.py S3.contacts.list S3.unique_windows.bed
+
+=====================================================================================================================
+"""
 import pandas as pd
 import sys
 import re
 if not len(sys.argv) == 3:
     print("extract_windows.py <file_to_extract> <output_file>")
     exit(-1)
+# Get specified piece of a window in the form chrA:1-2
 def get_start_loc(name, index):
     return re.split('[:-]', name)[index]
 out_filename = sys.argv[2]
-print(f'FILE: {sys.argv[1]}')
-# df = pd.read_csv(sys.argv[1], delim_whitespace="\t", names=["window1", "window2"])
 df = pd.read_csv(sys.argv[1], sep='\t', names=['window1', 'window2'])
-print(f'DF: {df}')
 windows = pd.concat([df['window1'],df['window2']], axis=0, ignore_index=True)
 print("Contact List Size: ", df.shape[0])
 
-# this math is not entirely accurate right?  Because if a window x appears in
-# window1 and window2, then it will be counted twice, but in unique_windows
-# it will only appear once - Michael
 print("Unique from window 1 list: ", len(df['window1'].unique()))
 print("Unique from window 2 list: ", len(df['window2'].unique()))
 unique_windows = windows.unique()
